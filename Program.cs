685bb18c212c08670da44ce4cae4750bfbefd4e0
riasat <35196032+riasati@users.noreply.github.com>
2019-06-14 12:50:09 +0430

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TurningNPDAtoCFG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =int.Parse(Console.ReadLine());
+            Console.ReadLine();
+            Console.ReadLine();
+            Console.ReadLine();
+            LinkedList<string> strList = new LinkedList<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string str = "q" + i;
+                strList.AddLast(str);
+            }
+            string s = "";
+            string c = "";
+            while ((s = Console.ReadLine()) != "")
+            {
+                Rule a = new Rule(s);
+                c += a.RuternGrammer(strList);
+                c += "\n";
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ Rule.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TurningNPDAtoCFG
+{
+    public class Rule
+    {
+        public bool HaveFirstState;
+        public bool HaveFinalState;
+        public string FromState;
+        public string ToState;
+        public string FirstInput;
+        public string SecondInput;
+        public string Output;
+        public Rule(string str)
+        {
+            string[] strArr = str.Split(',');
+            if (strArr[0][0] == '-')
+            {
+                HaveFirstState = true;
+                FromState = strArr[0].Substring(2);
+            }
+            else
+            {
+                HaveFirstState = false;
+                FromState = strArr[0];
+            }
+            FirstInput = strArr[1];
+            SecondInput = strArr[2];
+            Output = strArr[3];
+            if (strArr[4][0] == '*')
+            {
+                HaveFinalState = true;
+                ToState = strArr[4].Substring(1);
+            }
+            else
+            {
+                HaveFinalState = false;
+                ToState = strArr[4];
+            }
+
+        }
+        public string RuternGrammer(LinkedList<String> States)
+        {
+            if (this.Output == "_")
+            {
+                return "(" + this.FromState + this.SecondInput + this.ToState + ")" + "->" + this.FirstInput;
+            }
+            string returnStr = "";
+            foreach (var x in States)
+            {
+                string str = "";
+                foreach (var y in States)
+                {
+                    str += "(" + this.FromState + this.SecondInput + x + ")" + "->" + this.FirstInput + "(" + this.ToState + this.Output[0] + y + ")" + "(" + y + this.Output[1] + x + ")";
+                    str += "|";
+                }
+                str = str.Substring(0, str.Length - 1);
+                returnStr += str + "\n";
+            }
+            returnStr = returnStr.Substring(0, returnStr.Length - 1);
+            return returnStr;
+        }
+    }
+}
